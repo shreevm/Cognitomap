8aa72ad40c5a170f54ac90f3e8cd68826ba13a03
V.SHREE VARAA MANGAI <75275922+shreevm@users.noreply.github.com>
2025-10-18 07:50:21 -0400

    commit

--- backend/app.py
+++ backend/app.py
@@ -1109,4 +1109,5 @@ def get_students():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
